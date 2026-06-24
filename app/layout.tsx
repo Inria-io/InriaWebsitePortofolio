8e0b9536fcd30fb43c6ebb0158ec81504696efee
inriakalalo37@gmail.com <inriakalalo4@gmail.com>
2026-06-25 02:22:09 +0700

    update: change website title to Inria Kalalo - Website Portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ const plusJakarta = Plus_Jakarta_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Inria's Portfolio | Tech Entrepreneur & Developer",
+  title: "Inria Kalalo - Website Portfolio | Tech Entrepreneur & Developer",
   description: "Hi, I'm Inria Altje Kalalo, an IT student from Indonesia with a deep passion for building technology that matters. Explore my projects, skills, and blog.",
 };
 
